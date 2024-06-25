2aade09aa010a8200d4150967c977dbe81a8f789
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-06-25 12:12:43 +0200

    CW - A wolf in sheep's clothing

--- Pages/8. 3. A wolf in sheep's clothing.xcplaygroundpage/Contents.swift
+++ Pages/8. 3. A wolf in sheep's clothing.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
-//: [Previous](@previous)
+/**
+ 
+ DESCRIPTION:
+ Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+ 
+ Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+ 
+ [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+ 7      6      5      4      3            2      1
+ 
+ If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+ 
+ Note: there will always be exactly one wolf in the array.
+ 
+ Examples
+ Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+ Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+ 
+ Input: ["sheep", "sheep", "wolf"]
+ Output: "Pls go away and stop eating my sheep"
+ 
+ **/
 
-import Foundation
+func warnTheSheep(_ queue: [String]) -> String {
+    guard var wolfPosition = queue.firstIndex(of: "wolf") else { return "wolf not found in sequence" }
+    
+    if wolfPosition == queue.count - 1 {
+        return "Pls go away and stop eating my sheep"
+    } else  {
+        let sheepPosition = queue.count - wolfPosition - 1
+        return "Oi! Sheep number \(sheepPosition)! You are about to be eaten by a wolf!"
+    }
+}
 
-var greeting = "Hello, playground"
+print(warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep"]))
 
-//: [Next](@next)
+// sheep position is counted from the right. (Farmers point of view)
