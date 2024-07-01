5e2d5b2790fad48faa2331097039e5eaa7d15e8a
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-07-01 16:04:39 +0200

    CW - Multiplication table for number

---
+++ Pages/8. 6. Multiplication table for number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+/**
+ 
+ DESCRIPTION:
+ Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+ For example, a multiplication table (string) for number == 5 looks like below:
+ 
+ 1 * 5 = 5
+ 2 * 5 = 10
+ 3 * 5 = 15
+ 4 * 5 = 20
+ 5 * 5 = 25
+ 6 * 5 = 30
+ 7 * 5 = 35
+ 8 * 5 = 40
+ 9 * 5 = 45
+ 10 * 5 = 50
+ 
+ **/
+
+
+func multi_table(_ number: Int) -> String {
+
+    let result = (1...10).map { "\($0) * \(number) = \($0 * number)"  }
+    return result.joined(separator: "\n")
+    
+}
+
+print(multi_table(15))
